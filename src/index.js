4d487c866983f1019944173566ba4631f059ddf1
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-10-02 02:48:42 -0300

    separating application

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-const restify = require('restify')
-const server = restify.createServer()
-
-server.get('/', (req, res, next) => {
-  res.send('Enjoy Silence')
-})
+const server = require('./server')
 
 server.listen('3456')
