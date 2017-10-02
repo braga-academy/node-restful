3f7a1a4777ac20c8e846ca79f993ac0a9f4712ec
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-10-02 03:14:53 -0300

    Category Methods API

--- src/http/routes.js
+++ src/http/routes.js
@@ -1,8 +1,27 @@
+
 const routes = (server) => {
   server.get('/', (req, res, next) => {
     res.send('Enjoy the Silence!!')
     next()
   })
+
+  server.get('category', (req, res, next) => {
+    res.send(['1', 'Test Category'])
+    next()
+  })
+  server.post('category', (req, res, next) => {
+    const { name } = req.params
+    res.send(name)
+    next()
+  })
+  // server.put('category', (req, res, next) => {
+  //   res.send()
+  //   next()
+  // })
+  // server.delete('category', (req, res, next) => {
+  //   res.send()
+  //   next()
+  // })
 }
 
 module.exports = routes
--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,7 @@ const cors = require('./cors')
 
 server.pre(cors.preflight)
 server.use(cors.actual)
+server.use(restify.plugins.bodyParser())
 
 routes(server)
 
